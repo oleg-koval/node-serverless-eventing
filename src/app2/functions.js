6994cb2ff71025e7f6108fefef9c775ede78f1e3
Oleg Koval <kvl.olg@gmail.com>
2019-06-11 02:39:55 +0200

    refactor: remove previous handlers code

--- src/app2/functions.js
+++
@@ -1,3 +0,0 @@
-module.exports.function5 = async () => {
-    // I'll consume the DisburseLoan command and dispatch a LoanDisbursed event
-}
--- src/app2/functions.spec.js
+++
@@ -1,7 +0,0 @@
-var { function5 } = require('./functions')
-
-describe('functions', () => {
-    it('function5', async () => {
-        expect(true).toBe(true)
-    })
-})
