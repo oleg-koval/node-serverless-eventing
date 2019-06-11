239aa1ebde7577258d40474a448193c5232aba87
Oleg Koval <kvl.olg@gmail.com>
2019-06-11 02:41:56 +0200

    refactor: add create handler

---
+++ src/handlers/offer/create.js
@@ -0,0 +1,16 @@
+const LoanModel = require('../../models/loan')
+const { send, error } = require('../../utils/response')
+const { parseWith } = require('../../utils/request')
+
+module.exports.handler = async event => {
+    const parseJson = parseWith(JSON.parse)
+    const input = parseJson(event.pathParameters)
+
+    try {
+        const loan = await LoanModel()
+        const createdLoan = await loan(input, 'create')
+        return send(createdLoan)
+    } catch (err) {
+        return error(err)
+    }
+}
