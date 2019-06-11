7d73c784a7a97f1709a65bcb0df9d26567b0d1d6
Oleg Koval <kvl.olg@gmail.com>
2019-06-11 13:25:24 +0200

    other: add global env conf for jest

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,7 @@ module.exports = {
     testEnvironment: 'node',
     roots: ['<rootDir>/src/'],
     verbose: true,
+    globals: {
+        NODE_ENV: 'development',
+    },
 }
--- src/config/index.spec.js
+++ src/config/index.spec.js
@@ -1,16 +1,19 @@
 const config = require('./')
 
-const OLD_ENV = process.env
+// const OLD_ENV = process.env
 
-beforeEach(() => {
-    jest.resetModules()
-    process.env = { ...OLD_ENV }
-    delete process.env.NODE_ENV
-})
+// beforeEach(() => {
+//     jest.resetModules()
+//     process.env = { ...OLD_ENV }
+//     delete process.env.NODE_ENV
+// })
 
-afterEach(() => {
-    process.env = OLD_ENV
-})
+// afterEach(() => {
+//     process.env = OLD_ENV
+// })
+
+// eslint-disable-next-line no-console
+console.log(process.env.NODE_ENV, '>>>>>>>>>')
 
 describe('Gets configuration based on environment', () => {
     it('should get configuration for development env', () => {
