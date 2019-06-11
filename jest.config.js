61dc0026cbc969db6814709d92f9ff9e0824e205
Oleg Koval <kvl.olg@gmail.com>
2019-06-11 15:43:43 +0200

    fix(folder name): update src -> lib name change

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     testEnvironment: 'node',
-    roots: ['<rootDir>/src/'],
+    roots: ['<rootDir>/lib/'],
     verbose: true,
     globals: {
         NODE_ENV: 'development',
