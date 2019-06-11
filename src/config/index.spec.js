59464437cb205664b1bd7de94738088cdf4902fb
Oleg Koval <kvl.olg@gmail.com>
2019-06-11 11:58:00 +0200

    test: fix env in config test

--- src/config/index.spec.js
+++ src/config/index.spec.js
@@ -1,10 +1,20 @@
 const config = require('./')
 
-afterEach(() => {
-    process.env.NODE_ENV = 'development'
+const OLD_ENV = process.env
+
+beforeEach(() => {
+    jest.resetModules()
+    process.env = { ...OLD_ENV }
+    delete process.env.NODE_ENV
 })
+
+afterEach(() => {
+    process.env = OLD_ENV
+})
+
 describe('Gets configuration based on environment', () => {
     it('should get configuration for development env', () => {
+        process.env.NODE_ENV = 'development'
         const result = config()
 
         expect(result).toMatchSnapshot()
